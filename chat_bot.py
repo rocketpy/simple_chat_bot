c83bc0aeec158ffa4fa931c3f7fc5f76b2d065de
rocketpy <dimitr.ushakov@gmail.com>
2020-09-14 16:25:56 +0300

    added a main code

--- chat_bot.py
+++ chat_bot.py
@@ -1,1 +1,14 @@
+# pip install chatterbot
+# pip install chatterbot_corpus
 
+
+from chatterbot import ChatBot
+
+
+bot = ChatBot('BotName')
+
+bot = ChatBot(
+    'BotName',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database_uri='sqlite:///database.sqlite3'
+)

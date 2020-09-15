766e47bb283d5dbbf49bd481039dbb1a8394daf3
rocketpy <dimitr.ushakov@gmail.com>
2020-09-15 22:31:21 +0300

    added a usage part

--- chat_bot.py
+++ chat_bot.py
@@ -40,5 +40,16 @@ trainer.train([
 
 #  testing
 response = bot.get_response('I have a ...')
-
 print("Bot response:", response)
+
+#  usage
+name=input("Enter Your Name: ")
+print("Welcome to the Bot Service! Let me know how can I help you?")
+while True:
+    request=input(name+':')
+    if request=='Bye' or request =='bye':
+        print('Bot: Bye')
+        break
+    else:
+        response=bot.get_response(request)
+        print('Bot:',response)
